3d17c0027b1d1fc00fc6a5f33490a817cffb8fc8
CJ Harries <cj@wizardsoftheweb.pro>
2023-12-02 09:13:02 -0600

    Implement part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -41,12 +41,12 @@ fn parse_part1_game(input: &str) -> Part1Game {
         .parse::<u32>()
         .unwrap();
     let games = parts.next().unwrap();
-    let mut games = games.split("; ");
+    let games = games.split("; ");
     let mut max_green = 0;
     let mut max_red = 0;
     let mut max_blue = 0;
     for game in games {
-        let mut colors = game.split(", ");
+        let colors = game.split(", ");
         for color in colors {
             let mut color = color.split(" ");
             let count = color.next().unwrap().parse::<u32>().unwrap();
@@ -94,8 +94,13 @@ fn part1(input: String) -> u32 {
         .sum()
 }
 
-fn part2(input: String) -> String {
-    todo!()
+fn part2(input: String) -> u32 {
+    input
+        .trim()
+        .lines()
+        .map(parse_part1_game)
+        .map(|game| game.max_green * game.max_red * game.max_blue)
+        .sum()
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -129,6 +134,22 @@ mod tests {
         Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
         Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
         "
+                .to_string()
+            )
+        );
+    }
+
+    #[test]
+    fn solves_part2() {
+        assert_eq!(
+            2286,
+            part2(
+                "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+                "
                 .to_string()
             )
         );
