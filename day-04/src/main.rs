afa48eb74533a64e8dad596dcf73a629c41644a3
CJ Harries <cj@wizardsoftheweb.pro>
2023-12-04 18:05:41 -0600

    Test part 1

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -21,11 +21,11 @@ fn main() {
     println!("Part 2: {}", part2(input));
 }
 
-fn part1(input: String) -> String {
+fn part1(input: String) -> u32 {
     todo!()
 }
 
-fn part2(input: String) -> String {
+fn part2(input: String) -> u32 {
     todo!()
 }
 
@@ -33,4 +33,21 @@ fn part2(input: String) -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn solves_part1() {
+        assert_eq!(
+            13,
+            part1(
+                "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        "
+                .to_string()
+            )
+        );
+    }
 }
