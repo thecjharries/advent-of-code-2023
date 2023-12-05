c3d916a793beccf2d27e8cbfa042ece1609097e6
CJ Harries <cj@wizardsoftheweb.pro>
2023-12-05 17:58:01 -0600

    Implement part 1

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -26,12 +26,14 @@ fn parse_to_map(input: &str) -> BTreeMap<usize, usize> {
     let input = input.trim();
     let mut result = BTreeMap::new();
     for line in input.lines() {
+        if line.ends_with(':') {
+            continue;
+        }
         let line = line.trim();
         let parts = line.split_whitespace().collect::<Vec<&str>>();
         let value = parts[0].parse::<usize>().unwrap();
         let key = parts[1].parse::<usize>().unwrap();
         let max = parts[2].parse::<usize>().unwrap();
-
         for index in 0..max {
             result.insert(key + index, value + index);
         }
@@ -50,6 +52,7 @@ fn parse_seeds(input: &str) -> Vec<usize> {
 #[derive(Debug, PartialEq)]
 struct Garden {
     seeds: Vec<usize>,
+    seed_to_soil: BTreeMap<usize, usize>,
     soil_to_fertilizer: BTreeMap<usize, usize>,
     fertilizer_to_water: BTreeMap<usize, usize>,
     water_to_light: BTreeMap<usize, usize>,
@@ -59,7 +62,30 @@ struct Garden {
 }
 
 fn part1(input: String) -> usize {
-    todo!()
+    let input = input.trim();
+    let mut chunks = input.split("\n\n");
+    let seeds = parse_seeds(chunks.next().unwrap());
+    let seed_to_soil = parse_to_map(chunks.next().unwrap());
+    let soil_to_fertilizer = parse_to_map(chunks.next().unwrap());
+    let fertilizer_to_water = parse_to_map(chunks.next().unwrap());
+    let water_to_light = parse_to_map(chunks.next().unwrap());
+    let light_to_temperature = parse_to_map(chunks.next().unwrap());
+    let temperature_to_humidity = parse_to_map(chunks.next().unwrap());
+    let humidity_to_location = parse_to_map(chunks.next().unwrap());
+    seeds
+        .iter()
+        .map(|seed| {
+            let soil = seed_to_soil.get(&seed).unwrap_or(&seed);
+            let fertilizer = soil_to_fertilizer.get(&soil).unwrap_or(&soil);
+            let water = fertilizer_to_water.get(&fertilizer).unwrap_or(&fertilizer);
+            let light = water_to_light.get(&water).unwrap_or(&water);
+            let temperature = light_to_temperature.get(&light).unwrap_or(&light);
+            let humidity = temperature_to_humidity
+                .get(&temperature)
+                .unwrap_or(&temperature);
+            humidity_to_location.get(&humidity).unwrap_or(&humidity)
+        })
+        .fold(usize::MAX, |acc, location| acc.min(*location))
 }
 
 fn part2(input: String) -> usize {
@@ -138,5 +164,49 @@ mod tests {
     #[test]
     fn parses_seeds() {
         assert_eq!(vec![79, 14, 55, 13], parse_seeds("seeds: 79 14 55 13"));
+    }
+
+    #[test]
+    fn solves_part1() {
+        assert_eq!(
+            35,
+            part1(
+                "seeds: 79 14 55 13
+
+                seed-to-soil map:
+                50 98 2
+                52 50 48
+
+                soil-to-fertilizer map:
+                0 15 37
+                37 52 2
+                39 0 15
+
+                fertilizer-to-water map:
+                49 53 8
+                0 11 42
+                42 0 7
+                57 7 4
+
+                water-to-light map:
+                88 18 7
+                18 25 70
+
+                light-to-temperature map:
+                45 77 23
+                81 45 19
+                68 64 13
+
+                temperature-to-humidity map:
+                0 69 1
+                1 0 69
+
+                humidity-to-location map:
+                60 56 37
+                56 93 4
+                "
+                .to_string()
+            )
+        )
     }
 }
