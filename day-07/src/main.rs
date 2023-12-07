04644e459fd0f71be5cd92292bb01528c84604ac
CJ Harries <cj@wizardsoftheweb.pro>
2023-12-07 12:46:43 -0600

    Implement and test parsing cards from chars

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -21,7 +21,8 @@ fn main() {
     println!("Part 2: {}", part2(input));
 }
 
-enum Hands {
+#[derive(Debug, PartialEq)]
+enum HandRanking {
     HighCard = 0,
     OnePair = 1,
     TwoPairs = 2,
@@ -31,6 +32,7 @@ enum Hands {
     FiveOfAKind = 6,
 }
 
+#[derive(Debug, PartialEq)]
 enum Card {
     Two = 2,
     Three = 3,
@@ -40,11 +42,32 @@ enum Card {
     Seven = 7,
     Eight = 8,
     Nine = 9,
-    Ten = 10, // T
-    Jack = 11, // J
+    Ten = 10,   // T
+    Jack = 11,  // J
     Queen = 12, // Q
-    King = 13, // K
-    Ace = 14, // A
+    King = 13,  // K
+    Ace = 14,   // A
+}
+
+impl Card {
+    fn from_char(character: char) -> Option<Card> {
+        match character {
+            '2' => Some(Card::Two),
+            '3' => Some(Card::Three),
+            '4' => Some(Card::Four),
+            '5' => Some(Card::Five),
+            '6' => Some(Card::Six),
+            '7' => Some(Card::Seven),
+            '8' => Some(Card::Eight),
+            '9' => Some(Card::Nine),
+            'T' => Some(Card::Ten),
+            'J' => Some(Card::Jack),
+            'Q' => Some(Card::Queen),
+            'K' => Some(Card::King),
+            'A' => Some(Card::Ace),
+            _ => None,
+        }
+    }
 }
 
 fn part1(input: String) -> usize {
@@ -59,4 +82,22 @@ fn part2(input: String) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn can_parse_card_from_input() {
+        assert_eq!(Some(Card::Two), Card::from_char('2'));
+        assert_eq!(Some(Card::Three), Card::from_char('3'));
+        assert_eq!(Some(Card::Four), Card::from_char('4'));
+        assert_eq!(Some(Card::Five), Card::from_char('5'));
+        assert_eq!(Some(Card::Six), Card::from_char('6'));
+        assert_eq!(Some(Card::Seven), Card::from_char('7'));
+        assert_eq!(Some(Card::Eight), Card::from_char('8'));
+        assert_eq!(Some(Card::Nine), Card::from_char('9'));
+        assert_eq!(Some(Card::Ten), Card::from_char('T'));
+        assert_eq!(Some(Card::Jack), Card::from_char('J'));
+        assert_eq!(Some(Card::Queen), Card::from_char('Q'));
+        assert_eq!(Some(Card::King), Card::from_char('K'));
+        assert_eq!(Some(Card::Ace), Card::from_char('A'));
+        assert_eq!(None, Card::from_char('X'));
+    }
 }
