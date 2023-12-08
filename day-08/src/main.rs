21d210a2a09d7e566ec9b526269f8c6c18bd98fb
CJ Harries <cj@wizardsoftheweb.pro>
2023-12-08 15:19:47 -0600

    Test part 2

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -183,4 +183,25 @@ mod tests {
             )
         );
     }
+
+    #[test]
+    fn solves_part2() {
+        assert_eq!(
+            6,
+            part2(
+                "LR
+
+                11A = (11B, XXX)
+                11B = (XXX, 11Z)
+                11Z = (11B, XXX)
+                22A = (22B, XXX)
+                22B = (22C, 22C)
+                22C = (22Z, 22Z)
+                22Z = (22B, 22B)
+                XXX = (XXX, XXX)
+                "
+                .to_string()
+            )
+        );
+    }
 }
