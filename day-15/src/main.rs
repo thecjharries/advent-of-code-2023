abdbb17d068cf956c4915e5320bdc2b32cea2907
CJ Harries <cj@wizardsoftheweb.pro>
2023-12-15 12:29:41 -0600

    Implement test for reindeer hash

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -21,6 +21,10 @@ fn main() {
     println!("Part 2: {}", part2(input));
 }
 
+fn reindeer_hash(input: &str) -> usize {
+    todo!()
+}
+
 fn part1(input: String) -> usize {
     todo!()
 }
@@ -33,4 +37,30 @@ fn part2(input: String) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn reindeer_hash_returns_expected() {
+        // rn=1 becomes 30.
+        assert_eq!(30, reindeer_hash("rn=1"));
+        // cm- becomes 253.
+        assert_eq!(253, reindeer_hash("cm-"));
+        // qp=3 becomes 97.
+        assert_eq!(97, reindeer_hash("qp=3"));
+        // cm=2 becomes 47.
+        assert_eq!(47, reindeer_hash("cm=2"));
+        // qp- becomes 14.
+        assert_eq!(14, reindeer_hash("qp-"));
+        // pc=4 becomes 180.
+        assert_eq!(180, reindeer_hash("pc=4"));
+        // ot=9 becomes 9.
+        assert_eq!(9, reindeer_hash("ot=9"));
+        // ab=5 becomes 197.
+        assert_eq!(197, reindeer_hash("ab=5"));
+        // pc- becomes 48.
+        assert_eq!(48, reindeer_hash("pc-"));
+        // pc=6 becomes 214.
+        assert_eq!(214, reindeer_hash("pc=6"));
+        // ot=7 becomes 231.
+        assert_eq!(231, reindeer_hash("ot=7"));
+    }
 }
