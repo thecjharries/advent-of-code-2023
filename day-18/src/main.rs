96cc8e0d07148a8809f4bebb3de20b03fb141688
CJ Harries <cj@wizardsoftheweb.pro>
2023-12-18 16:20:40 -0600

    Create Direction enum

--- day-18/src/main.rs
+++ day-18/src/main.rs
@@ -21,6 +21,14 @@ fn main() {
     println!("Part 2: {}", part2(input));
 }
 
+#[derive(Debug, PartialEq, Eq)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
 fn part1(input: String) -> usize {
     todo!()
 }
@@ -33,4 +41,29 @@ fn part2(input: String) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn solves_part1() {
+        assert_eq!(
+            62,
+            part1(
+                "R 6 (#70c710)
+                D 5 (#0dc571)
+                L 2 (#5713f0)
+                D 2 (#d2c081)
+                R 2 (#59c680)
+                D 2 (#411b91)
+                L 5 (#8ceee2)
+                U 2 (#caa173)
+                L 1 (#1b58a2)
+                U 2 (#caa171)
+                R 2 (#7807d2)
+                U 3 (#a77fa3)
+                L 2 (#015232)
+                U 2 (#7a21e3)
+                "
+                .to_string()
+            )
+        );
+    }
 }
