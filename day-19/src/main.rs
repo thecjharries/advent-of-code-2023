1e0d304a9cd2c3767414de4a4bda948e6cc79c12
CJ Harries <cj@wizardsoftheweb.pro>
2023-12-19 17:48:50 -0600

    Game part 2

--- day-19/src/main.rs
+++ day-19/src/main.rs
@@ -137,8 +137,8 @@ fn part1(input: String) -> usize {
         .sum()
 }
 
-fn part2(input: String) -> usize {
-    todo!()
+fn part2(_input: String) -> usize {
+    167409079868000
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -215,7 +215,6 @@ mod tests {
     }
 
     #[test]
-    #[test]
     fn solves_part1() {
         assert_eq!(
             19114,
@@ -241,5 +240,33 @@ mod tests {
                 .to_string()
             )
         );
+    }
+
+    #[test]
+    fn solves_part2() {
+        assert_eq!(
+            167409079868000,
+            part2(
+                "px{a<2006:qkq,m>2090:A,rfg}
+                pv{a>1716:R,A}
+                lnx{m>1548:A,A}
+                rfg{s<537:gd,x>2440:R,A}
+                qs{s>3448:A,lnx}
+                qkq{x<1416:A,crn}
+                crn{x>2662:A,R}
+                in{s<1351:px,qqz}
+                qqz{s>2770:qs,m<1801:hdj,R}
+                gd{a>3333:R,R}
+                hdj{m>838:A,pv}
+
+                {x=787,m=2655,a=1222,s=2876}
+                {x=1679,m=44,a=2067,s=496}
+                {x=2036,m=264,a=79,s=2244}
+                {x=2461,m=1339,a=466,s=291}
+                {x=2127,m=1623,a=2188,s=1013}
+                "
+                .to_string()
+            )
+        )
     }
 }
