f6338dbd9a824cf40718bef54a0266936a905347
CJ Harries <cj@wizardsoftheweb.pro>
2023-12-24 13:11:46 -0600

    Parse Hailstone from input structure

--- day-24/src/main.rs
+++ day-24/src/main.rs
@@ -27,6 +27,29 @@ struct Hailstone {
     velocity: (isize, isize, isize),
 }
 
+impl Hailstone {
+    fn from_str(input: &str) -> Self {
+        let input = input.trim();
+        let mut parts = input.split(" @");
+        let position = parts
+            .next()
+            .expect("Unable to get position")
+            .split(',')
+            .map(|x| x.trim().parse::<isize>().expect("Unable to parse position"))
+            .collect::<Vec<isize>>();
+        let velocity = parts
+            .next()
+            .expect("Unable to get velocity")
+            .split(',')
+            .map(|x| x.trim().parse::<isize>().expect("Unable to parse velocity"))
+            .collect::<Vec<isize>>();
+        Self {
+            position: (position[0], position[1], position[2]),
+            velocity: (velocity[0], velocity[1], velocity[2]),
+        }
+    }
+}
+
 fn part1(input: String) -> usize {
     todo!()
 }
@@ -39,6 +62,17 @@ fn part2(input: String) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn hailstone_parses_from_input() {
+        assert_eq!(
+            Hailstone {
+                position: (19, 13, 30),
+                velocity: (-2, 1, -2)
+            },
+            Hailstone::from_str("19, 13, 30 @ -2,  1, -2")
+        );
+    }
 
     #[test]
     fn solves_part1() {
