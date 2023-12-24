17b9e92557a5b63613cd80cd3506c483a8068556
CJ Harries <cj@wizardsoftheweb.pro>
2023-12-24 13:09:09 -0600

    Test part 1

--- day-24/src/main.rs
+++ day-24/src/main.rs
@@ -33,4 +33,19 @@ fn part2(input: String) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn solves_part1() {
+        assert_eq!(
+            2,
+            part1(
+                "19, 13, 30 @ -2,  1, -2
+                18, 19, 22 @ -1, -1, -2
+                20, 25, 34 @ -2, -2, -4
+                12, 31, 28 @ -1, -2, -1
+                20, 19, 15 @  1, -5, -3
+                ".to_string()
+            )
+        )
+    }
 }
